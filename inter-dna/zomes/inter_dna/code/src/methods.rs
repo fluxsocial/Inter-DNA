910fe902bfc70a0986be9aac3f8fcac77ff0bec5
jdeepee <joshuadparkin@gmail.com>
2020-04-28 16:30:03 +0100

    fix hash generation from global entry ref

--- inter-dna/zomes/inter_dna/code/src/methods.rs
+++ inter-dna/zomes/inter_dna/code/src/methods.rs
@@ -1,24 +1,31 @@
+use hdk::holochain_persistence_api::hash::HashString;
 use hdk::{
     error::{ZomeApiError, ZomeApiResult},
     holochain_json_api::json::JsonString,
     prelude::{Address, Entry, GetLinksOptions, LinkMatch, Pagination, SizePagination, SortOrder},
 };
 use meta_traits::{GlobalEntryRef, InterDNADao};
-use std::convert::TryInto;
+use multihash::Hash;
 
 use crate::InterDNA;
 
 impl InterDNADao for InterDNA {
     fn create_link(source: GlobalEntryRef, target: GlobalEntryRef) -> ZomeApiResult<()> {
-        let source_address: Address = JsonString::from(source.clone()).try_into()?;
+        let source_entry = Entry::App("global_entry_ref".into(), source.into());
+        let source_address: Address = HashString::encode_from_json_string(
+            JsonString::from(source_entry.clone()),
+            Hash::SHA2256,
+        );
         if hdk::get_entry(&source_address)?.is_none() {
-            let source_entry = Entry::App("global_entry_ref".into(), source.into());
             hdk::commit_entry(&source_entry)?;
         };
 
-        let target_address: Address = JsonString::from(target.clone()).try_into()?;
+        let target_entry = Entry::App("global_entry_ref".into(), target.into());
+        let target_address: Address = HashString::encode_from_json_string(
+            JsonString::from(target_entry.clone()),
+            Hash::SHA2256,
+        );
         if hdk::get_entry(&target_address)?.is_none() {
-            let target_entry = Entry::App("global_entry_ref".into(), target.into());
             hdk::commit_entry(&target_entry)?;
         };
 
@@ -28,12 +35,18 @@ impl InterDNADao for InterDNA {
     }
 
     fn remove_link(source: GlobalEntryRef, target: GlobalEntryRef) -> ZomeApiResult<()> {
-        let source_address: Address = JsonString::from(source.clone()).try_into()?;
+        let source_address: Address = HashString::encode_from_json_string(
+            JsonString::from(Entry::App("global_entry_ref".into(), source.into())),
+            Hash::SHA2256,
+        );
         hdk::get_entry(&source_address)?.ok_or(ZomeApiError::Internal(String::from(
             "Source entry does not exist",
         )))?;
 
-        let target_address: Address = JsonString::from(target.clone()).try_into()?;
+        let target_address: Address = HashString::encode_from_json_string(
+            JsonString::from(Entry::App("global_entry_ref".into(), target.into())),
+            Hash::SHA2256,
+        );
         hdk::get_entry(&target_address)?.ok_or(ZomeApiError::Internal(String::from(
             "Target entry does not exist",
         )))?;
@@ -47,7 +60,10 @@ impl InterDNADao for InterDNA {
         count: usize,
         page: usize,
     ) -> ZomeApiResult<Vec<GlobalEntryRef>> {
-        let source_address: Address = JsonString::from(source.clone()).try_into()?;
+        let source_address: Address = HashString::encode_from_json_string(
+            JsonString::from(Entry::App("global_entry_ref".into(), source.into())),
+            Hash::SHA2256,
+        );
         Ok(hdk::get_links_with_options(
             &source_address,
             LinkMatch::Any,
@@ -74,7 +90,10 @@ impl InterDNADao for InterDNA {
         count: usize,
         page: usize,
     ) -> ZomeApiResult<Vec<GlobalEntryRef>> {
-        let target_address: Address = JsonString::from(target.clone()).try_into()?;
+        let target_address: Address = HashString::encode_from_json_string(
+            JsonString::from(Entry::App("global_entry_ref".into(), target.into())),
+            Hash::SHA2256,
+        );
         Ok(hdk::get_links_with_options(
             &target_address,
             LinkMatch::Exactly("incoming"),
